7d756bc81289bc21b2307a845dadc130094358c5
hantomayel27-creator <hantomayel27@icloud.com>
2026-06-10 16:40:40 +0100

    📁 REFF : Dossier templates connecté pour Render

--- mentorlink/settings.py
+++ mentorlink/settings.py
@@ -48,7 +48,8 @@ ROOT_URLCONF = 'mentorlink.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
