52b300e1d148089536fe74f23a8d998c82674897
hantomayel27-creator <hantomayel27@icloud.com>
2026-06-10 16:55:17 +0100

    ⚙️ URLS : Correction de l'importation de views

--- mentorlink/urls.py
+++ mentorlink/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from core import views  # 🚨 Ligne obligatoire pour éviter l'erreur "views is not defined" !
+from core import views  # Ligne indispensable
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.page_accueil_redirection, name='accueil_login'),  # Route vers la page de connexion
-    path('api/', include('core.urls')),  # Vos routes d'API existantes
+    path('', views.page_accueil_redirection, name='accueil_login'),
+    path('api/', include('core.urls')),
 ]
