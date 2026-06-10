beff77bd61fd12dbafaae9a961da81005cf45745
hantomayel27-creator <hantomayel27@icloud.com>
2026-06-10 15:58:50 +0100

    📦 CONFIG : Préparation pour le déploiement sur Render

--- mentorlink/settings.py
+++ mentorlink/settings.py
@@ -4,9 +4,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-=^1ft(%38m-*=2na1z3u-0rx$3x2lhs(lpj&txo+cn$pho1zlp'
 
-DEBUG = True
 
+import os
+import dj_database_url
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-=^1ft(%38m-*=2na1z3u-0rx$3x2lhs(lpj&txo+cn$pho1zlp')
+
+# Désactive le mode debug en production pour la sécurité
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
+# Autorise toutes les adresses de Render
 ALLOWED_HOSTS = ['*']
+
 CSRF_TRUSTED_ORIGINS = ['https://backspace-decathlon-unroasted.ngrok-free.dev']
 
 INSTALLED_APPS = [
@@ -24,6 +33,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # Indispensable en premier pour ton frontend !
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Sert à servir les fichiers statiques en production
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -31,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'mentorlink.urls'
 
@@ -53,11 +64,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mentorlink.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
+
+# Configuration des fichiers statiques pour la production
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
